d45fe24e5670f037124551fd934727ba8d32b618
Carlos Chan <181876093+carlosmendez-dev@users.noreply.github.com>
2024-11-30 20:30:04 -0600

    Cree las migraciones de las siguientes tablas: - mesas - establecimientos - clientes - platos

---
+++ database/migrations/2024_12_01_022232_create_establecimientos_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('establecimientos', function (Blueprint $table) {
+            $table->id();
+            $table->text("nombre");
+            $table->text("numero");
+            $table->text("direccion");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('establecimientos');
+    }
+};
