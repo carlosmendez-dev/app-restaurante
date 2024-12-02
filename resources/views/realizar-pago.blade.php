b6d9d6afca93b8e6b872a405e019abb578c042ad
Israel Martinez <190049477+IsraelMartinez007@users.noreply.github.com>
2024-12-01 19:16:58 -0600

    ya esta listo lo de realizar pago

--- resources/views/realizar-pago.blade.php
+++ resources/views/realizar-pago.blade.php
@@ -3,22 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seleccionar Sucursal</title>
+    <title>Realizar pago</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-screen bg-gray-100 flex justify-center items-center">
     <x-ui.card>
-        <form action="" class="flex justify-center items-center flex-col gap-3 w-full">
+        <form action="" class="flex justify-center items-center flex-col gap-3 w-full" x-data="{pago:10, total:150}">
             <h1 class="text-center text-xl">Realizar Pago</h1> <br>
             <div class="w-full flex flex-col gap-4">
                 <label class="text-left">Total</label>
-                <input type="text" placeholder="$150.00" class="w-full border border-gray-300 rounded px-2 py-1 ">
+                <input type="text" :value="total" class="w-full border border-gray-300 rounded px-2 py-1" disabled>
                 <label class="text-left">Pago</label>
-                <input type="text" placeholder="$500.00" class="w-full border border-gray-300 rounded px-2 py-1">
+                <input type="text"  x-model="pago" class="w-full border border-gray-300 rounded px-2 py-1">
+                <p x-show="pago < total" class="text-red-500">falta money</p>
                 <label class="text-left">Cambio</label>
-                <input type="text" placeholder="$350.00" class="w-full border border-gray-300 rounded px-2 py-1">
+                <input type="text" :value="pago - total" class="w-full border border-gray-300 rounded px-2 py-1">
             </div>
-            <x-ui.btn>Pagar</x-ui.btn>
+            <div x-show="pago >= total" class="w-full">
+             <x-ui.btn>Pagar</x-ui.btn>
+            </div>
             <a href="#" class="text-sm text-blue-500 hover:underline mt-3">Cancelar</a>
         </form>
     </x-ui.card>
