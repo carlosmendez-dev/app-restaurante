6250928532c2d98500673b4d63670657c21abc90
Israel Martinez <190049477+IsraelMartinez007@users.noreply.github.com>
2024-12-02 12:14:06 -0600

    me faltaban agregar unos modelos en los controladores, pero ya esta

--- app/Http/Controllers/EstablecimientoController.php
+++ app/Http/Controllers/EstablecimientoController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Establecimiento;
 use Illuminate\Http\Request;
+use App\Models\Establecimiento;
 
 class EstablecimientoController extends Controller
 {
